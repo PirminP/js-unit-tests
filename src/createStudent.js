8b3a32b2fd6f7f8b02160cfd178adb67a366decd
Pirmin Pernsteiner <pirmin.pernsteiner@gmail.com>
2022-03-08 20:00:46 -0300

    Finalize task 4

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let array = { name: nome };
+  const text = () => 'Eita pessoa boa!';
+  array.feedback = text;
+  return array;
+};
 
 module.exports = createStudent;
